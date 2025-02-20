6ac66233c17f66a597d1f730696b19f5db7e974f
Sashiy1 <aleksander.paluch@wp.pl>
2025-02-20 11:59:44 +0100

    routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/HomePage/HomePage";
 
-const App = () => {
+
+const About = lazy(() => import("./pages/AboutPage/AboutPage"));
+const Products = lazy(() => import("./pages/ProductsPage/ProductsPage"));
+const Careeer = lazy(() => import("./pages/CareerPage/CareerPage"));
+const Contact = lazy(() => import("./pages/ContactPage/ContactPage"));
+
+
+const App: React.FC = () => {
   return (
-    <div>
-        <h1 className=" font-bold text-amber-300 text-xl  underline">
-    Hello world!
-  </h1>
-    </div>
-  )
-}
+    <>
+      <Suspense fallback="loading">
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/about" element={<About/>} />
+          <Route path="/products" element={<Products/>} />
+          <Route path="/careeer" element={<Careeer/>} />
+          <Route path="/contact" element={<Contact/>} />
+        </Routes>
+      </Suspense>
+    </>
+  );
+};
 
-export default App
+export default App;
